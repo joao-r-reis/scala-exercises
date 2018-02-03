39ba9483d739a7049a68c8dc8451c8581ce9677f
joao.reis <joao.reis@farfetch.com>
2018-02-03 13:51:44 +0000

    "added duplicate test for experimentation reasons"

--- hello-world/src/test/scala/example/HelloSpec.scala
+++ hello-world/src/test/scala/example/HelloSpec.scala
@@ -6,4 +6,9 @@ class HelloSpec extends FlatSpec with Matchers {
   "The Hello object" should "say hello" in {
     Hello.greeting shouldEqual "hello"
   }
+
+  // just to see if I can put 2 tests in same spec class
+  "The Hello object2" should "say hello" in {
+    Hello.greeting shouldEqual "hello"
+  }
 }
